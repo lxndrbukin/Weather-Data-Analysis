ae03c5405b967ba235f92aa96607014dfd206ef2
lxndrbukin <lxndrbukin@proton.me>
2026-06-30 20:23:52 +0100

    First commit

---
+++ ingest_data.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+from datetime import datetime
+import pandas as pd
+import os
+import requests
+
+load_dotenv()
+
+WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+
+def fetch_data():
+    req = requests.get(
+        'https://api.openweathermap.org/data/2.5/weather',
+        params={
+            'q': 'London',
+            'appid': WEATHER_API_KEY,
+            'units': 'metric'
+        }
+    )
+
+    response = req.json()
+    df_normalized = pd.json_normalize(response)
+    df_normalized['weather_description'] = df_normalized['weather'].apply(lambda x: x[0]['description'])
+    df_normalized['weather_main'] = df_normalized['weather'].apply(lambda x: x[0]['main'])
+    df_normalized = df_normalized.drop(columns=['weather'])
+
+    df_normalized.columns = [col.replace('main.', '') for col in df_normalized.columns]
+    df_normalized.columns = [col.replace('wind.', 'wind_') for col in df_normalized.columns]
+    df_normalized.columns = [col.replace('clouds.', 'clouds_') for col in df_normalized.columns]
+    df_normalized.columns = [col.replace('sys.', '') for col in df_normalized.columns]
+    df_normalized.columns = [col.replace('coord.', '') for col in df_normalized.columns]
+    df_normalized['dt'] = df_normalized['dt'].apply(lambda x: datetime.fromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S'))
+
+    return df_normalized
+
+def save_local(df, dt):
+    path = f'data/date={dt}/weather_data.csv'
+    os.makedirs(f'data/date={dt}', exist_ok=True)
+    df.to_csv(path, index=False)
+    return path
+
+def main():
+    df = fetch_data()
+    dt = datetime.today().strftime('%Y-%m-%d')
+    path = save_local(df, dt)
+
+if __name__ == '__main__':
+    main()
